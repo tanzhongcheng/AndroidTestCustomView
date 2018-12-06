34eb7a00e0753d1e7a5f2aafbfdc2a21440fbfad
谭忠诚 <tanzhongcheng@tanzhonengdeMBP.lan>
2018-12-06 20:29:39 +0800

    add item

--- app/src/main/java/com/tan/androidcustomview/DeleteListItemActivity.java
+++ app/src/main/java/com/tan/androidcustomview/DeleteListItemActivity.java
@@ -52,5 +52,25 @@ public class DeleteListItemActivity extends AppCompatActivity {
         contentList.add("Content Item 18");
         contentList.add("Content Item 19");
         contentList.add("Content Item 20");
+        contentList.add("Content Item 1");
+        contentList.add("Content Item 2");
+        contentList.add("Content Item 3");
+        contentList.add("Content Item 4");
+        contentList.add("Content Item 5");
+        contentList.add("Content Item 6");
+        contentList.add("Content Item 7");
+        contentList.add("Content Item 8");
+        contentList.add("Content Item 9");
+        contentList.add("Content Item 10");
+        contentList.add("Content Item 11");
+        contentList.add("Content Item 12");
+        contentList.add("Content Item 13");
+        contentList.add("Content Item 14");
+        contentList.add("Content Item 15");
+        contentList.add("Content Item 16");
+        contentList.add("Content Item 17");
+        contentList.add("Content Item 18");
+        contentList.add("Content Item 19");
+        contentList.add("Content Item 20");
     }
 }
